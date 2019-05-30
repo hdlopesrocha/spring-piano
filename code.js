89a720f22932c634a52fff5cb2c75eaf596a5671
hdlopesrocha <hdlopesrocha91@gmail.com>
2019-05-30 15:23:08 +0200

    05 - Detune

--- code.js
+++ code.js
@@ -75,27 +75,42 @@ function onKeyPress(key, note) {
     var frequency = getFrequency(note);
     console.log('press', note, frequency);
     $("#key_" + key).addClass("pressed");
-
     var state = pianoState[note];
     if (!state) {
         var state = {
             oscillator1: getAutioContext().createOscillator(),
-            gain1: getAutioContext().createGain()
+            oscillator2: getAutioContext().createOscillator(),
+            gain1: getAutioContext().createGain(),
+            gain2: getAutioContext().createGain()
         };
 
         state.oscillator1.start(0);
+        state.oscillator2.start(0);
         state.gain1.gain.value = 0;
+        state.gain2.gain.value = 0;
 
         state.oscillator1.connect(state.gain1);
+        state.oscillator2.connect(state.gain2);
         state.gain1.connect(master);
+        state.gain2.connect(master);
 
         pianoState[note] = state;
     }
     var oscillator1 = state.oscillator1;
+    var oscillator2 = state.oscillator2;
     var gain1 = state.gain1;
+    var gain2 = state.gain2;
 
+    oscillator1.type = 'sawtooth';
+    oscillator2.type = 'sawtooth';
     oscillator1.frequency.setValueAtTime(frequency, time);
-    gain1.gain.setTargetAtTime(1.0, time, 0.25);
+    oscillator2.frequency.setValueAtTime(frequency, time);
+
+    oscillator2.detune.setValueAtTime(0.0, time);
+    oscillator2.detune.setTargetAtTime(frequency / 8, time, 1);
+    gain1.gain.setTargetAtTime(0.5, time, 0.25);
+    gain2.gain.setTargetAtTime(0.5, time, 0.25);
+
 }
 
 function onKeyRelease(key, note) {
@@ -103,12 +118,10 @@ function onKeyRelease(key, note) {
     var frequency = getFrequency(note);
     console.log('release', note, frequency);
     $("#key_" + key).removeClass("pressed");
-
     var state = pianoState[note];
     if (state) {
-        console.log('release', time, key, note, frequency);
-        var time = getAutioContext().currentTime;
         state.gain1.gain.setTargetAtTime(0.0, time, 0.25);
+        state.gain2.gain.setTargetAtTime(0.0, time, 0.25);
     }
 }
 
